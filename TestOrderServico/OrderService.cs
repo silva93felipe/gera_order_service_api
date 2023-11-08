2b802b679e301aa73f45d80807008cca42b21a66
Felipe Santos <silva93felipe@gmail.com>
2023-11-07 22:44:04 -0300

    test: implementando teste iniciais

--- TestOrderServico/OrderService.cs
+++ TestOrderServico/OrderService.cs
@@ -11,14 +11,32 @@ namespace TestOrderServico
     public class OrderService
     {
         [Fact]
-        public void Deveria_Criar_Uma_Order_Service_Com_Status_Aberto()
+        public void CreateOrdemServico_StatusEhAberto()
         {
+            // Arange
             var operadorAberturaId = Guid.NewGuid();
             var equipamentoId = Guid.NewGuid();
 
+            //Act
             var newOrderService = new OrdemServico(operadorAberturaId, equipamentoId);
 
+            //Assert
             Assert.True(newOrderService.Status == OrdermStatusEnum.Aberto);            
+        }
+        [Fact]
+        public void FinalizarOrdem_StatusEhFechado()
+        {
+            // Arange
+            var operadorAberturaId = Guid.NewGuid();
+            var operadorFechamentoId = Guid.NewGuid();
+            var equipamentoId = Guid.NewGuid();
+            var newOrderService = new OrdemServico(operadorAberturaId, equipamentoId);
+
+            //Act
+            newOrderService.FinalizarOrdem(operadorFechamentoId);
+
+            //Assert
+            Assert.True(newOrderService.Status == OrdermStatusEnum.Fechado);            
         }
     }
 }
